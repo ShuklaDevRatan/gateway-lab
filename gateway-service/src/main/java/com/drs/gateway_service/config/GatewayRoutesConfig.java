3b6743fd8e7c8a1b5c132f8af32ad4bef6b38670
Dev Ratan Shukla <panditdevratanshukla@gmail.com>
2026-08-23 21:49:54 +0530

    Add Eureka service discovery and Redis rate limiting

--- gateway-service/src/main/java/com/drs/gateway_service/config/GatewayRoutesConfig.java
+++ gateway-service/src/main/java/com/drs/gateway_service/config/GatewayRoutesConfig.java
@@ -1,12 +1,37 @@
 package com.drs.gateway_service.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class GatewayRoutesConfig {
+
+    @Value("${rate-limit.replenish-rate}")
+    private int replenishRate;
+
+    @Value("${rate-limit.burst-capacity}")
+    private int burstCapacity;
+
+    @Bean
+    public RedisRateLimiter redisRateLimiter() {
+        return new RedisRateLimiter(replenishRate , burstCapacity);
+    }
+
+    @Bean
+    public KeyResolver apiKeyResolver() {
+        return exchange ->
+                Mono.justOrEmpty(
+                        exchange.getRequest()
+                                .getHeaders()
+                                .getFirst("X-API-KEY")
+                );
+    }
 
     @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
@@ -15,10 +40,15 @@ public class GatewayRoutesConfig {
 
                 .route("user-service", route -> route
                         .path("/gateway/api/users/**")
-                        .filters(filter -> filter
-                                .stripPrefix(2)
+                        .filters(filters ->
+                                filters
+                                        .stripPrefix(2)
+                                        .requestRateLimiter(config -> config
+                                                .setRateLimiter(redisRateLimiter())
+                                                .setKeyResolver(apiKeyResolver())
+                                        )
                         )
-                        .uri("http://localhost:8081")
+                        .uri("lb://user-service")
                 )
 
                 .build();
